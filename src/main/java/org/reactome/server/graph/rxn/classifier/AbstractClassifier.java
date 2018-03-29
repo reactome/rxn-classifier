53047fc9e18dca2d6959b02fdd1e16480f6435e7
Antonio Fabregat <fabregat.antonio@gmail.com>
2018-03-29 10:43:02 +0100

    adding the unclassified reactions

--- src/main/java/org/reactome/server/graph/rxn/classifier/AbstractClassifier.java
+++ src/main/java/org/reactome/server/graph/rxn/classifier/AbstractClassifier.java
@@ -3,6 +3,7 @@ package org.reactome.server.graph.rxn.classifier;
 import org.apache.commons.lang3.StringUtils;
 import org.neo4j.ogm.model.Result;
 import org.reactome.server.graph.rxn.common.Classifier;
+import org.reactome.server.graph.rxn.common.RxnClassifier;
 import org.reactome.server.graph.rxn.utils.FileUtils;
 import org.reactome.server.graph.rxn.utils.MapSet;
 import org.reactome.server.graph.service.GeneralService;
@@ -18,7 +19,8 @@ import java.util.*;
  */
 public abstract class AbstractClassifier implements Classifier {
 
-    public static MapSet<String, String> classifications = new MapSet<>();
+    public static MapSet<String, String> classified = new MapSet<>();
+    public static Set<String> unclassified = new HashSet<>();
 
     @Override
     public String getName() {
@@ -52,41 +54,19 @@ public abstract class AbstractClassifier implements Classifier {
         lines.add("\"" + StringUtils.join(getHeader(), "\",\"") + "\"");
         for (Map<String, Object> map : result) {
             String stId = String.valueOf(map.get(getHeader().get(0)));
-            classifications.add(stId, getName());
+            if(getClass().getAnnotation(RxnClassifier.class)!=null) {
+                classified.add(stId, getName());
+            } else {
+                unclassified.add(stId);
+            }
 
             List<String> line = new ArrayList<>();
             for (String attribute : getHeader()) {
-                // Some results might be list of elements. In some cases we use REDUCE and the output looks like
-                // ["a", "b", "c", ] and we want it to look like ["a", "b", "c"].
-                //               ^ we remove this comma and the space after it
-                // That's why we replace ", ]" by "]"
-                Object aux = map.get(attribute);
-                if(aux instanceof Object[]){
-                    StringBuilder rtn = new StringBuilder("[");
-                    for (Object item : (Object[]) aux) {
-                        rtn.append(item).append(", ");
-                    }
-                    aux = rtn.append("]").toString();
-                }
-                line.add("\"" + (aux == null ? null : ("" + aux).replaceAll(", ]$", "]")) + "\"");
+                line.add(String.format("\"%s\"", map.get(attribute)));
             }
             lines.add(StringUtils.join(line, ","));
         }
         Files.write(path, lines, Charset.forName("UTF-8"));
         return lines.size() - 1;
-    }
-
-    public static void reportClassifications(String path, String name){
-        List<String> lines = new ArrayList<>();
-        for (String stId : classifications.keySet()) {
-            Set<String> classification = classifications.get(stId);
-            lines.add(String.format("%s,%d,\"%s\"", stId, classification.size(),StringUtils.join(classification, "\",\"")));
-        }
-
-        try {
-            Files.write(FileUtils.getFilePath(path, name), lines, Charset.forName("UTF-8"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
